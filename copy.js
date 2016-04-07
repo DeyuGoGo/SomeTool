3435de1767201e9ef9a1a04af658da5b3345fab9
DeyuGoGo <a0924xxx@gmail.com>
2016-04-07 17:51:18 +0800

    1.這個是複製那些很多dpi圖片時用的js file= =

---
+++ copy.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+
+var args = process.argv.slice(2);
+var sources = ['drawable-hdpi' , 'drawable-mdpi','drawable-xhdpi','drawable-xxhdpi','drawable-xxxhdpi'];
+// /Users/Deyu/githubs/ChuangGu-Android/ChuangGu/messager/src/main/res
+sources.forEach(function (val, index, array) {
+  mkdirp(args[1]+'/'+val+'/',function(err){});
+  copyFile('./'+val+'/'+args[0],args[1]+'/'+val+'/'+args[0]);
+});
+
+function copyFile(source, target,cb) {
+  var cbCalled = false;
+  console.log(source);
+  console.log(target);
+  var rd = fs.createReadStream(source);
+  rd.on("error", function(err) {
+    done(err);
+  });
+  var wr = fs.createWriteStream(target);
+  wr.on("error", function(err) {
+    done(err);
+  });
+  wr.on("close", function(ex) {
+    done();
+  });
+  rd.pipe(wr);
+
+  function done(err) {
+    if (!cbCalled) {
+      // cb(err);
+      cbCalled = true;
+    }
+  }
+}
